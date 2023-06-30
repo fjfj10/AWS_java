1447eb9ba86d06a1f98e5eada24379fa8b2e055a
fjfj10 <kimmisuk2218@gmail.com>
2023-06-30 16:00:24 +0900

    feat: 변수명을 instance로 변경하여 객체를 하나로 고정하는 싱글톤 작업진행

--- src/ch14_싱글톤/FactoyMain.java
+++ src/ch14_싱글톤/FactoyMain.java
@@ -2,10 +2,11 @@ package ch14_싱글톤;    //유일한 객체
 
 public class FactoyMain {
 	public static void main(String[] args) {
-		Samsung samsung = new Samsung();
+		Samsung samsung = Samsung.getInstance();		
+		System.out.println(samsung.getCompanyName());
 		
-		SamsungFactory factory1 = new SamsungFactory(samsung);
-		SamsungFactory factory2 = new SamsungFactory(samsung);
+		SamsungFactory factory1 = new SamsungFactory();
+		SamsungFactory factory2 = new SamsungFactory();
 		
 		factory1.produce("갤럭시s").showInfo();
 		factory2.produce("갤럭시s2").showInfo();
--- src/ch14_싱글톤/Samsung.java
+++ src/ch14_싱글톤/Samsung.java
@@ -1,14 +1,27 @@
 package ch14_싱글톤;
 
+import java.time.LocalDate;
+
 public class Samsung {
+	
+	private static Samsung instance;    //변수명 instance 고정  1
+	
 	private String companyName;
-	private int autoIncrementSerialNumber;
+	private int autoIncrementSerialNumber = LocalDate.now().getYear() * 10000;
 	
 	
 
-	public Samsung() {
-		super();
-		this.companyName = "Samsung";
+	private Samsung() {    //생성자는 private으로 고정    2
+		companyName = Samsung.class.getSimpleName().toUpperCase();
+		//		companyName = "Samsung";
+	}
+	
+	public static Samsung getInstance() {    // getInstance로 고정    3
+		if(instance == null) {
+			instance = new Samsung();
+		}
+		
+		return instance;
 	}
 	
 	public int getAutoIncrementSerialNumber() {
