1b8c09ed9f55420ee0e8bc7bb9223aaf1c668fb7
fjfj10 <kimmisuk2218@gmail.com>
2023-07-10 13:56:43 +0900

    feat: 2023.07.06.시험 해당 문제 풀이

--- src/Test/CalculatorMain.java
+++ src/Test/CalculatorMain.java
@@ -19,3 +19,37 @@ public class CalculatorMain {
 		Calculator.sub(200, 100);
 	}
 }
+//답
+//public class Calculator {
+//
+//    public int add(int num1, int num2) {
+//
+//        return num1 + num2;
+//
+//    }
+//
+// 
+//
+//    public int add(int num1, int num2) {
+//
+//        return num1 - num2;
+//
+//    }
+//
+//}
+//
+// 
+//
+//public class Main{
+//
+//    public static void main(String[] args) {
+//
+//        Calculator calculator = new Calculator();
+//
+//        System.out.println( calculator.add(10, 20) );
+//
+//        System.out.println( calculator.sub(200, 100) );
+//
+//    }
+//
+//}
--- src/Test/MapList.java
+++ src/Test/MapList.java
@@ -8,24 +8,28 @@ import java.util.Map.Entry;
 
 public class MapList {
 	public static void main(String[] args) {
-		Map<String, Object> strMap1 = new HashMap<String, Object>();
-		strMap1.put("name", "홍길동");
-		strMap1.put("rating", "vip");
-		strMap1.put("age", "30");
-		Map<String, Object> strMap2 = new HashMap<String, Object>();
-		strMap2.put("name", "김기영");
-		strMap2.put("rating", "gold");
-		strMap2.put("age", "35");
-//나중에 답 확인	
-//		strMap.add()
-//		for(Map<String, Object> customer : customers) {
-//				for(Entry<String, Object> entry : strMap.entrySet()) {
-//					System.out.println(entry.getKey());
-//					System.out.println(entry.getValue());
-//					
-//				}
-//			
-//		}
+		List<Map<String, Object>> customers = new ArrayList<Map<String, Object>>();
+		
+		Map<String, Object> customer1 = new HashMap<String, Object>();
+		customer1.put("name", "홍길동");
+		customer1.put("rating", "vip");
+		customer1.put("age", "30");
+		
+		customers.add(customer1);
+		
+		Map<String, Object> customer2 = new HashMap<String, Object>();
+		customer2.put("name", "김기영");
+		customer2.put("rating", "gold");
+		customer2.put("age", "35");
+		
+		customers.add(customer2);
+		
+		for(Map<String, Object> customer : customers) {
+			for(Map.Entry<String, Object> entry : customer.entrySet()) {
+				System.out.println(entry);
+			}
+		}
+
 	}
 }
 //내답 
@@ -50,9 +54,3 @@ public class MapList {
 //		}
 //	}
 //}
-//name=홍길동 
-//rating=vip
-//age=30
-//name=김기영
-//rating=gold
-//age=35
